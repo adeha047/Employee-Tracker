2b873e5d9cdc95bf7d9f9d1d44ae143c1e5dc648
adeha047 <dehaven7777@gmail.com>
2021-03-07 19:36:43 -0500

    added new tables a seed folder

--- server.js
+++ server.js
@@ -8,10 +8,34 @@ const connection = mysql.createConnection({
     user: "root",
     password: "rootroot",
     port: 3306,
-    database: "greatbay_db"
+    database: "employee_db"
 });
 
 connection.connect( (err) => {
     if(err) throw err;
-    askFirstQuestions();
+    beginPrompt();
 });
+
+function beginPrompt() {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "How would you like to begin?",
+            name: "choices",
+            choices: ["View Employees", "BID", "EXIT"]
+        }
+    ])
+
+    
+
+}
+
+
+// * The command-line application should allow users to:
+
+//   * Add departments, roles, employees
+
+//   * View departments, roles, employees
+
+//   * Update employee roles
